f318d3e9caf22679f3f38b75bfbdac886f952a6a
REZ4-source <rezaa.1387a@gmail.com>
2026-06-10 11:12:42 +0330

    Update data.js

--- data/data.js
+++ data/data.js
@@ -1,4 +1,38 @@
 const animeData = [
+
+ {
+            id: "one-piece",
+            title: "One Piece S2",
+            fullTitle: "One Pice S2",
+            description: "ماجراجویی لوفی و دوستانش برای پیدا کردن گنج وان پیس و رسیدن به رویای مشترکشون آزادی و تبدیل شدن به پادشاه دزدان دریایی. سفری حماسی پر از خنده، اشک، نبرد و رفاقت‌های موندگار.",
+            genres: ["فانتزی", "ماجراجویی", "اکشن"],
+            imdbRating: "9",
+            malRating: "8.73",
+            poster: "https://i.postimg.cc/dQrpqVvJ/42c4e1708c4136f128ecb57a084bee19.jpg",
+            horizontalCover: "https://i.postimg.cc/dQrpqVvJ/42c4e1708c4136f128ecb57a084bee19.jpg",
+            verticalCover: "https://i.postimg.cc/cJvkPsTc/0793036344891436c2a1b23df20c97e1.jpg",
+            trailer: "http://anjs11.animeonapp.com/anjsdl2/1434659607842-pgv4ql-1564065260545.mp4",
+            seasons: [
+                {
+                    seasonNumber: 2,
+                    seasonName: "فصل دوم - درحال پخش",
+                    episodes: [
+                        { epNum: 1156, qualities: { "480p": "", "720p": "http://anjs33.animeonapp.com/anjsdl22/One.Piece.E1156.720p.WEB-DL.x264.SoftSub.AnimeON.mkv" } },
+                        { epNum: 1157, qualities: { "480p": "", "720p": "http://anjs33.animeonapp.com/anjsdl22/One.Piece.E1157.720p.WEB-DL.x264.SoftSub.AnimeON.mkv" } },
+                        { epNum: 1158, qualities: { "480p": "", "720p": "http://anjs33.animeonapp.com/anjsdl22/One.Piece.E1158.720p.WEB-DL.x264.SoftSub.AnimeON.mkv" } },
+                        { epNum: 1159, qualities: { "480p": "", "720p": "http://anjs33.animeonapp.com/anjsdl22/One.Piece.E1159.720p.WEB-DL.x264.SoftSub.AnimeON.mkv" } },
+                        { epNum: 1160, qualities: { "480p": "", "720p": "http://anjs33.animeonapp.com/anjsdl22/One.Piece.E1160.720p.WEB-DL.x264.SoftSub.AnimeON.mkv" } },
+                        { epNum: 1161, qualities: { "480p": "", "720p": "http://anjs33.animeonapp.com/anjsdl22/One.Piece.E1161.720p.WEB-DL.x264.SoftSub.AnimeON.mkv" } },
+                        { epNum: 1162, qualities: { "480p": "", "720p": "http://anjs33.animeonapp.com/anjsdl22/One.Piece.E1162.720p.WEB-DL.x264-AnimeON.mkv" } },
+                        { epNum: 1163, qualities: { "480p": "", "720p": "http://anjs33.animeonapp.com/anjsdl22/One.Piece.1163.720p.WEB-DL.SoftSub.AnimeON.mkv" } },
+                        { epNum: 1164, qualities: { "480p": "", "720p": "http://anjs33.animeonapp.com/anjsdl22/One.Piece.1164.720p.WEB-DL.SoftSub.AnimeON.mkv" } },
+                        { epNum: 1165, qualities: { "480p": "", "720p": "http://anjs33.animeonapp.com/anjsdl22/One.Piece.E1165.720p.WEB-DL.x264.SoftSub.AnimeON.mkv" } },
+                        { epNum: 1166, qualities: { "480p": "", "720p": "" } }
+                        
+                    ]
+                }
+            ]
+         },
   
      {
             id: "marriagetoxin",
