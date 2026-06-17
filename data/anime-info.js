d2334ff6418076ae07304f0a92bb7a87415fc6d9
REZ4-source <rezaa.1387a@gmail.com>
2026-06-17 07:31:33 +0330

    Add files via upload

---
+++ data/anime-info.js
@@ -0,0 +1,500 @@
+// data/anime-info.js
+const animeInfo = {
+    // هر آیدی = آیدی همون انیمه توی دیتای اصلی
+    
+   "Smoking-Behind-the-Supermarket-with-You": {
+        "year": "۲۰۲۶",
+        "episodes": "۱۲",
+        "status": "در حال پخش",
+        "studio": "Asahi Production",
+        "duration": "۲۴ دقیقه",
+        "season": "تابستان ۲۰۲۶",
+        "source": "مانگا",
+        "ageRating": "+۱۳",
+        "director": "تاداتو سوزوکی",
+        "language": "ژاپنی",
+        "note": "زیرنویس"
+    },
+    
+    "one-piece": {
+        "year": "۱۹۹۹",
+        "episodes": "+۱۱۳۷",
+        "status": "در حال پخش",
+        "studio": "توئی انیمیشن",
+        "duration": "۲۴ دقیقه",
+        "season": "پاییز ۱۹۹۹",
+        "source": "مانگا",
+        "ageRating": "+۱۳",
+        "director": "کونوسوکه اودا",
+        "language": "ژاپنی",
+        "note": "زیرنویس"
+    },
+    
+  "marriagetoxin": {
+        "year": "۲۰۲۶",
+        "episodes": "۱۲",
+        "status": "در حال پخش",
+        "studio": "BONES Film",
+        "duration": "۲۴ دقیقه",
+        "season": "بهار ۲۰۲۶",
+        "source": "مانگا",
+        "ageRating": "+۱۳",
+        "director": "موتونوبو هوری",
+        "language": "ژاپنی",
+        "note": "زیرنویس"
+    },
+    
+  "witch-hat-atelier": {
+        "year": "۲۰۲۶",
+        "episodes": "۱۳",
+        "status": "در حال پخش",
+        "studio": "BUG FILMS",
+        "duration": "۲۴ دقیقه",
+        "season": "بهار ۲۰۲۶",
+        "source": "مانگا",
+        "ageRating": "+۱۳",
+        "director": "آیومو واتانابه",
+        "language": "ژاپنی",
+        "note": "زیرنویس"
+    },
+    
+    "The-Angel-Next-Door-Spoils-Me-Rotten": {
+        "year": "۲۰۲۳",
+        "episodes": "۲۴",
+        "status": "در حال پخش",
+        "studio": "project No.9",
+        "duration": "۲۴ دقیقه",
+        "season": "زمستان ۲۰۲۳",
+        "source": "لایت ناول",
+        "ageRating": "+۱۳",
+        "director": "کنیچی ایمایزومی",
+        "language": "ژاپنی",
+        "note": "زیرنویس"
+    },
+  
+  "Classroom-of-the-Elite": {
+        "year": "۲۰۱۷",
+        "episodes": "۳۸",
+        "status": "در حال پخش",
+        "studio": "Lerche",
+        "duration": "۲۴ دقیقه",
+        "season": "تابستان ۲۰۱۷",
+        "source": "لایت ناول",
+        "ageRating": "+۱۳",
+        "director": "سیجی کیشی",
+        "language": "ژاپنی",
+        "note": "زیرنویس"
+    },
+  
+  "devil-may-cry": {
+    year: 2025,
+    episodes: 8,
+    status: "پایان یافته",
+    studio: "Studio Mir",
+    duration: "21 تا 32 دقیقه",
+    season: "بهار",
+    source: "بازی ویدیویی",
+    ageRating: "R",
+    director: "Adi Shankar",
+    language: "انگلیسی",
+    note: "زیرنویس"
+},
+
+  "Invincible": {
+    year: 2021,
+    episodes: 24,
+    status: "پایان یافته",
+    studio: "Skybound Animation",
+    duration: "42 تا 55 دقیقه",
+    season: "بهار",
+    source: "کمیک",
+    ageRating: "TV-MA",
+    director: "Robert Valley",
+    language: "انگلیسی",
+    note: "زیرنویس"
+},
+
+"Arcane": {
+    year: 2021,
+    episodes: 18,
+    status: "پایان یافته",
+    studio: "Fortiche",
+    duration: "39 تا 50 دقیقه",
+    season: "پاییز",
+    source: "بازی ویدیویی",
+    ageRating: "TV-14",
+    director: "Pascal Charrue",
+    language: "انگلیسی",
+    note: "زیرنویس"
+},
+
+"lazarus-2025": {
+    year: 2025,
+    episodes: 13,
+    status: "پایان یافته",
+    studio: "MAPPA",
+    duration: "24 دقیقه",
+    season: "بهار",
+    source: "اورجینال",
+    ageRating: "R-17+",
+    director: "Shinichiro Watanabe",
+    language: "ژاپنی",
+    note: "زیرنویس"
+},
+
+"Cyberpunk-Edgerunners": {
+    year: 2022,
+    episodes: 10,
+    status: "پایان یافته",
+    studio: "Trigger",
+    duration: "24 دقیقه",
+    season: "پاییز",
+    source: "بازی ویدیویی",
+    ageRating: "R-17+",
+    director: "Hiroyuki Imaishi",
+    language: "ژاپنی",
+    note: "زیرنویس"
+},
+  
+  "sentenced-to-be-a-hero": {
+    year: 2025,
+    episodes: 13,
+    status: "پایان یافته",
+    studio: "Studio KAI",
+    duration: "24 دقیقه",
+    season: "پاییز",
+    source: "لایت ناول",
+    ageRating: "R-17+",
+    director: "Hiroyuki Takashima",
+    language: "ژاپنی",
+    note: "زیرنویس"
+},
+
+"Gachiakuta": {
+    year: 2025,
+    episodes: 24,
+    status: "پایان یافته",
+    studio: "Bones Film",
+    duration: "24 دقیقه",
+    season: "تابستان",
+    source: "مانگا",
+    ageRating: "PG-13",
+    director: "Fumihiko Suganuma",
+    language: "ژاپنی",
+    note: "زیرنویس"
+},
+
+"Dandadan": {
+    year: 2024,
+    episodes: 24,
+    status: "پایان یافته",
+    studio: "Science SARU",
+    duration: "24 دقیقه",
+    season: "پاییز",
+    source: "مانگا",
+    ageRating: "R-17+",
+    director: "Fuga Yamashiro",
+    language: "ژاپنی",
+    note: "زیرنویس"
+},
+
+"My-Dress-Up-Darling": {
+    year: 2022,
+    episodes: 12,
+    status: "پایان یافته",
+    studio: "CloverWorks",
+    duration: "24 دقیقه",
+    season: "زمستان",
+    source: "مانگا",
+    ageRating: "PG-13",
+    director: "Keisuke Shinohara",
+    language: "ژاپنی",
+    note: "زیرنویس"
+},
+
+"The-Fragrant-Flower-Blooms-with-Dignity": {
+    year: 2025,
+    episodes: 13,
+    status: "پایان یافته",
+    studio: "CloverWorks",
+    duration: "24 دقیقه",
+    season: "تابستان",
+    source: "مانگا",
+    ageRating: "PG-13",
+    director: "Miyuki Kuroki",
+    language: "ژاپنی",
+    note: "زیرنویس"
+},
+ 
+ "Kaiju-No.8": {
+    year: 2024,
+    episodes: 24,
+    status: "پایان یافته",
+    studio: "Production I.G",
+    duration: "24 دقیقه",
+    season: "بهار",
+    source: "مانگا",
+    ageRating: "PG-13",
+    director: "Shigeyuki Miya",
+    language: "ژاپنی",
+    note: "زیرنویس"
+},
+
+"To-Be-Hero-X": {
+    year: 2025,
+    episodes: 24,
+    status: "پایان یافته",
+    studio: "BeDream",
+    duration: "24 دقیقه",
+    season: "بهار",
+    source: "اورجینال",
+    ageRating: "PG-13",
+    director: "Li Haoling",
+    language: "چینی",
+    note: "زیرنویس"
+},
+
+"Dead-Account": {
+    year: 2026,
+    episodes: 12,
+    status: "پایان یافته",
+    studio: "SynergySP",
+    duration: "24 دقیقه",
+    season: "نامشخص",
+    source: "مانگا",
+    ageRating: "PG-13",
+    director: "Keiya Saito",
+    language: "ژاپنی",
+    note: "زیرنویس"
+},
+
+"Solo-Leveling": {
+    year: 2024,
+    episodes: 25,
+    status: "پایان یافته",
+    studio: "A-1 Pictures",
+    duration: "24 دقیقه",
+    season: "زمستان",
+    source: "مانهوا",
+    ageRating: "R-17+",
+    director: "Shunsuke Nakashige",
+    language: "ژاپنی",
+    note: "زیرنویس"
+},
+
+"Kaguya-sama": {
+    year: 2019,
+    episodes: 37,
+    status: "پایان یافته",
+    studio: "A-1 Pictures",
+    duration: "24 دقیقه",
+    season: "زمستان",
+    source: "مانگا",
+    ageRating: "PG-13",
+    director: "Mamoru Hatakeyama",
+    language: "ژاپنی",
+    note: "زیرنویس"
+}, 
+  
+  "May-I-Ask-for-One-Final-Thing": {
+    year: 2025,
+    episodes: 12,
+    status: "پایان یافته",
+    studio: "LIDENFILMS",
+    duration: "24 دقیقه",
+    season: "پاییز",
+    source: "لایت ناول",
+    ageRating: "PG-13",
+    director: "Kazuya Sakamoto",
+    language: "ژاپنی",
+    note: "زیرنویس"
+},
+
+"This-Monster-Wants-to-Eat-Me": {
+    year: 2025,
+    episodes: 12,
+    status: "پایان یافته",
+    studio: "Studio Lings",
+    duration: "24 دقیقه",
+    season: "پاییز",
+    source: "مانگا",
+    ageRating: "PG-13",
+    director: "Naoyuki Kuzuya",
+    language: "ژاپنی",
+    note: "زیرنویس"
+},
+
+"My-Status-as-an-Assassin-Obviously-Exceeds-the-Heros": {
+    year: 2025,
+    episodes: 12,
+    status: "پایان یافته",
+    studio: "Sunrise",
+    duration: "24 دقیقه",
+    season: "پاییز",
+    source: "لایت ناول",
+    ageRating: "PG-13",
+    director: "Nobuyoshi Habara",
+    language: "ژاپنی",
+    note: "زیرنویس"
+},
+
+"Sanda": {
+    year: 2025,
+    episodes: 12,
+    status: "پایان یافته",
+    studio: "Science SARU",
+    duration: "24 دقیقه",
+    season: "پاییز",
+    source: "مانگا",
+    ageRating: "PG-13",
+    director: "Tomohisa Shimoyama",
+    language: "ژاپنی",
+    note: "زیرنویس"
+},
+
+"Sakamoto-Days": {
+    year: 2025,
+    episodes: 22,
+    status: "پایان یافته",
+    studio: "TMS Entertainment",
+    duration: "24 دقیقه",
+    season: "زمستان",
+    source: "مانگا",
+    ageRating: "PG-13",
+    director: "Masaki Watanabe",
+    language: "ژاپنی",
+    note: "زیرنویس"
+},
+
+"Wind-Breaker": {
+    year: 2024,
+    episodes: 25,
+    status: "پایان یافته",
+    studio: "CloverWorks",
+    duration: "24 دقیقه",
+    season: "بهار",
+    source: "مانگا",
+    ageRating: "PG-13",
+    director: "Toshifumi Akai",
+    language: "ژاپنی",
+    note: "زیرنویس"
+},
+
+"Lord-of-Mysteries": {
+    year: 2025,
+    episodes: 13,
+    status: "پایان یافته",
+    studio: "BCMAY Pictures",
+    duration: "35 دقیقه",
+    season: "تابستان",
+    source: "وب ناول",
+    ageRating: "PG-13",
+    director: "Xiong Ke",
+    language: "چینی",
+    note: "زیرنویس"
+},
+
+"The-Apothecary-Diaries": {
+    year: 2023,
+    episodes: 48,
+    status: "پایان یافته",
+    studio: "TOHO Animation Studio",
+    duration: "24 دقیقه",
+    season: "پاییز",
+    source: "لایت ناول",
+    ageRating: "PG-13",
+    director: "Norihiro Naganuma",
+    language: "ژاپنی",
+    note: "زیرنویس"
+},
+
+"Chained-Soldier": {
+    year: 2024,
+    episodes: 12,
+    status: "پایان یافته",
+    studio: "Seven Arcs",
+    duration: "24 دقیقه",
+    season: "زمستان",
+    source: "مانگا",
+    ageRating: "R-17+",
+    director: "Gorou Kuji",
+    language: "ژاپنی",
+    note: "زیرنویس"
+},
+
+"Hells-Paradise": {
+    year: 2023,
+    episodes: 13,
+    status: "پایان یافته",
+    studio: "MAPPA",
+    duration: "24 دقیقه",
+    season: "بهار",
+    source: "مانگا",
+    ageRating: "R-17+",
+    director: "Kaori Makita",
+    language: "ژاپنی",
+    note: "زیرنویس"
+},
+
+"Jujutsu-Kaisen": {
+    year: 2020,
+    episodes: 47,
+    status: "پایان یافته",
+    studio: "MAPPA",
+    duration: "24 دقیقه",
+    season: "پاییز",
+    source: "مانگا",
+    ageRating: "R-17+",
+    director: "Sunghoo Park",
+    language: "ژاپنی",
+    note: "زیرنویس"
+},
+
+"Rascal-Does-Not-Dream-of-Bunny-Girl": {
+    year: 2018,
+    episodes: 13,
+    status: "پایان یافته",
+    studio: "CloverWorks",
+    duration: "24 دقیقه",
+    season: "پاییز",
+    source: "لایت ناول",
+    ageRating: "PG-13",
+    director: "Soichi Masui",
+    language: "ژاپنی",
+    note: "زیرنویس"
+},
+
+"Fire-Force": {
+    year: 2019,
+    episodes: 72,
+    status: "پایان یافته",
+    studio: "David Production",
+    duration: "24 دقیقه",
+    season: "تابستان",
+    source: "مانگا",
+    ageRating: "PG-13",
+    director: "Yuki Yase",
+    language: "ژاپنی",
+    note: "زیرنویس"
+},
+
+"Grand-blue": {
+    year: 2018,
+    episodes: 24,
+    status: "پایان یافته",
+    studio: "Zero-G",
+    duration: "24 دقیقه",
+    season: "تابستان",
+    source: "مانگا",
+    ageRating: "PG-13",
+    director: "Shinji Takamatsu",
+    language: "ژاپنی",
+    note: "زیرنویس"
+},
+  
+  
+  
+  
+  
+  
+    }
