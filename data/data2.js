548f08c1bf4485e5133828383816750bb8707666
REZ4-source <rezaa.1387a@gmail.com>
2026-06-24 20:28:48 +0330

    Update data2.js

--- data/data2.js
+++ data/data2.js
@@ -19,7 +19,7 @@ const animeData2 = [
             seasons: [
                 {
                     seasonNumber: 1,
-                    seasonName: "فصل اول - درحال پخش",
+                    seasonName: "فصل اول - 12 قسمت",
                     episodes: [
                         { epNum: 1, qualities: { "480p": "https://bnrhd3fvb374s4bkwzxm54xf.abrtech.top/pnot/Series/Marriagetoxin/Marriagetoxin.S01E01.mkv", "720p": "https://bnrhd3fvb374s4bkwzxm54xf.abrtech.top/pnot/Series/Marriagetoxin/Marriagetoxin.S01E01.WEB-DL.720p.Film2Media.mkv" } },
                         { epNum: 2, qualities: { "480p": "https://bnrhd3fvb374s4bkwzxm54xf.abrtech.top/pnot/Series/Marriagetoxin/Marriagetoxin.S01E02.mkv", "720p": "https://bnrhd3fvb374s4bkwzxm54xf.abrtech.top/pnot/Series/Marriagetoxin/Marriagetoxin.S01E02.WEB-DL.720p.Film2Media.mkv" } },
