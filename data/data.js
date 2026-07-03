ec8baef2cde9cc18ee776bfce08d6579e981bdc5
REZ4-source <rezaa.1387a@gmail.com>
2026-07-03 11:37:12 +0330

    Update data.js

--- data/data.js
+++ data/data.js
@@ -34,40 +34,7 @@ const animeData = [
                 }
             ]
          },
-     {
-            "id": "The-Wrong-Way-to-Use-Healing-Magic",
-            "title": "The Wrong Way to Use Healing Magic",
-            "fullTitle": "روش اشتباه استفاده از جادوی درمانی",
-            "description": "داستان درباره کنیچی کن آساگوری، دانش‌آموز دبیرستانی معمولی، است که به همراه دوستش سوزوکی و رئیس شورای دانش‌آموزی، رز، به دنیایی دیگر احضار می‌شوند. سوزوکی و رز یک قهرمان و شاهدخت هستند، اما کن به طور تصادفی قدرت کمیاب جادوی شفابخش را به دست می‌آورد – قدرتی که معمولاً بسیار ضعیف در نظر گرفته می‌شود.",
-            "genres": ["فانتزی", "کمدی", "اکشن"],
-            "imdbRating": "7.4",
-            "malRating": "7.25",
-            "poster": "https://i.postimg.cc/sXxwWbrZ/656042fc0545a3d5c4447bf44ced67f8.jpg",
-            "horizontalCover": "https://i.postimg.cc/sXxwWbrZ/656042fc0545a3d5c4447bf44ced67f8.jpg",
-            "verticalCover": "https://i.postimg.cc/pLMGS0D3/ea280b868a85165690dee3304b5dba71.jpg",
-            "trailer": "http://anjs44.animeonapp.com/anjsdl15/TheWrongWaytoUseHealingMagic-Officialtrailer.mp4",
-            "seasons": [
-                {
-                    "seasonNumber": 1,
-                    "seasonName": "فصل اول - 13 قسمت",
-                    "episodes": [
-                        { "epNum": 1, "qualities": { "480p": "http://anjs44.animeonapp.com/anjsdl15/ChiyuMahounoMachigattaTsukaikata-01-480p--AnimeON-.mkv", "720p": "http://anjs44.animeonapp.com/anjsdl15/ChiyuMahounoMachigattaTsukaikata-01-720p--AnimeON-.mkv" } },
-                        { "epNum": 2, "qualities": { "480p": "http://anjs44.animeonapp.com/anjsdl15/ChiyuMahounoMachigattaTsukaikata-02-480p--AnimeON-.mkv", "720p": "http://anjs44.animeonapp.com/anjsdl15/ChiyuMahounoMachigattaTsukaikata-02-720p--AnimeON-.mkv" } },
-                        { "epNum": 3, "qualities": { "480p": "http://anjs44.animeonapp.com/anjsdl15/ChiyuMahounoMachigattaTsukaikata-03-480p--AnimeON-.mkv", "720p": "http://anjs44.animeonapp.com/anjsdl15/ChiyuMahounoMachigattaTsukaikata-03-720p--AnimeON-.mkv" } },
-                        { "epNum": 4, "qualities": { "480p": "http://anjs44.animeonapp.com/anjsdl15/ChiyuMahounoMachigattaTsukaikata-04-480p--AnimeON-.mkv", "720p": "http://anjs44.animeonapp.com/anjsdl15/ChiyuMahounoMachigattaTsukaikata-04-720p--AnimeON-.mkv" } },
-                        { "epNum": 5, "qualities": { "480p": "http://anjs44.animeonapp.com/anjsdl15/ChiyuMahounoMachigattaTsukaikata-05-480p--AnimeON-.mkv", "720p": "http://anjs44.animeonapp.com/anjsdl15/ChiyuMahounoMachigattaTsukaikata-05-720p--AnimeON-.mkv" } },
-                        { "epNum": 6, "qualities": { "480p": "http://anjs44.animeonapp.com/anjsdl15/ChiyuMahounoMachigattaTsukaikata-06-480p--AnimeON-.mkv", "720p": "http://anjs44.animeonapp.com/anjsdl15/ChiyuMahounoMachigattaTsukaikata-06-720p--AnimeON-.mkv" } },
-                        { "epNum": 7, "qualities": { "480p": "http://anjs44.animeonapp.com/anjsdl15/ChiyuMahounoMachigattaTsukaikata-07-480p--AnimeON-.mkv", "720p": "http://anjs44.animeonapp.com/anjsdl15/ChiyuMahounoMachigattaTsukaikata-07-720p--AnimeON-.mkv" } },
-                        { "epNum": 8, "qualities": { "480p": "http://anjs44.animeonapp.com/anjsdl15/ChiyuMahounoMachigattaTsukaikata-08-480p--AnimeON-.mkv", "720p": "http://anjs44.animeonapp.com/anjsdl15/ChiyuMahounoMachigattaTsukaikata-08-720p--AnimeON-.mkv" } },
-                        { "epNum": 9, "qualities": { "480p": "http://anjs44.animeonapp.com/anjsdl15/ChiyuMahounoMachigattaTsukaikata-09-480p--AnimeON-.mkv", "720p": "http://anjs44.animeonapp.com/anjsdl15/ChiyuMahounoMachigattaTsukaikata-09-480p--AnimeON-.mkv" } },
-                        { "epNum": 10, "qualities": { "480p": "http://anjs44.animeonapp.com/anjsdl15/ChiyuMahounoMachigattaTsukaikata-10-480p--AnimeON-.mkv", "720p": "http://anjs44.animeonapp.com/anjsdl15/ChiyuMahounoMachigattaTsukaikata-10-720p--AnimeON-.mkv" } },
-                        { "epNum": 11, "qualities": { "480p": "http://anjs44.animeonapp.com/anjsdl15/ChiyuMahounoMachigattaTsukaikata-11-480p--AnimeON-.mkv", "720p": "http://anjs44.animeonapp.com/anjsdl15/ChiyuMahounoMachigattaTsukaikata-11-720p--AnimeON-.mkv" } },
-                        { "epNum": 12, "qualities": { "480p": "http://anjs44.animeonapp.com/anjsdl15/ChiyuMahounoMachigattaTsukaikata-12-480p--AnimeON-.mkv", "720p": "http://anjs44.animeonapp.com/anjsdl15/ChiyuMahounoMachigattaTsukaikata-12-720p--AnimeON-.mkv" } },
-                        { "epNum": 13, "qualities": { "480p": "http://anjs44.animeonapp.com/anjsdl15/ChiyuMahounoMachigattaTsukaikata-13-480p--AnimeON-.mkv", "720p": "http://anjs44.animeonapp.com/anjsdl15/ChiyuMahounoMachigattaTsukaikata-13-720p--AnimeON-.mkv" } }
-                    ]
-                }
-            ]
-        },
+
         
 
 
