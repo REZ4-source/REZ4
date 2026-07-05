0229b5e1797578982f06f969f45494d55b2f2155
REZ4 <rezaa.1387a@gmail.com>
2026-07-05 08:40:08 +0330

    Update data3.js

--- data/data3.js
+++ data/data3.js
@@ -1,6 +1,57 @@
 const animeData3 = [
    
-   
+   {
+            "id": "Wistoria-Wand-and-Sword",
+            "title": "Wistoria: Wand and Sword",
+            "fullTitle": "ویستوریا: عصا و شمشیر",
+            "description": "آکادمی جادو جای ضعیف‌ها نیست، مخصوصاً اگر حتی نتوانید یک جرقه‌ی ساده با چوب‌دستی‌تان ایجاد کنید! اما این پسر نوجوان برای جادو کردن نیامده؛ او آمده تا با قدرت خالص فیزیکی و یک شمشیر برنده، تمام قوانین دنیای جادو را به چالش بکشد. او یک قول مقدس به عزیزترین دوستش داده و هیچ طلسمی نمی‌تواند مانع او شود.",
+            "genres": ["فانتزی", "ماجراجویی", "اکشن"],
+            "imdbRating": "7.7",
+            "malRating": "?",
+            "poster": "https://intocdn.top/wp-content/uploads/2026/04/n4VljCX22hbw3RwfxaY8FX8eLzN.jpg",
+            "horizontalCover": "https://intocdn.top/wp-content/uploads/2026/04/n4VljCX22hbw3RwfxaY8FX8eLzN.jpg",
+            "verticalCover": "https://intocdn.top/wp-content/uploads/2026/04/5n0IZQA9MiXn5JBKZibtNhhFSAZ-360x480.jpg",
+            "trailer": "https://edge42.166864.ir.cdn.ir/Series/Tsue.to.Tsurugi.no.Wistoria/S01/Wistoria-Wand-and-Sword-Official-Main-Tr_Media.mp4",
+            "seasons": [
+                {
+                    "seasonNumber": 1,
+                    "seasonName": "فصل اول - 12 قسمت",
+                    "episodes": [
+                        { "epNum": 1, "qualities": { "480p": "https://bnrhd3fvb374s4bkwzxm54xf.abrtech.top/pnot/Series/Tsue.to.Tsurugi.no.Wistoria/S01/Tsue.to.Tsurugi.no.Wistoria.S01E01.mkv", "720p": "https://bnrhd3fvb374s4bkwzxm54xf.abrtech.top/pnot/Series/Tsue.to.Tsurugi.no.Wistoria/S01/Tsue.to.Tsurugi.no.Wistoria.S01E01.720p.WEB-DL.Farsi.Sub.Film2Media.mkv" } },
+                        { "epNum": 2, "qualities": { "480p": "https://bnrhd3fvb374s4bkwzxm54xf.abrtech.top/pnot/Series/Tsue.to.Tsurugi.no.Wistoria/S01/Tsue.to.Tsurugi.no.Wistoria.S01E02.mkv", "720p": "https://bnrhd3fvb374s4bkwzxm54xf.abrtech.top/pnot/Series/Tsue.to.Tsurugi.no.Wistoria/S01/Tsue.to.Tsurugi.no.Wistoria.S01E02.720p.WEB-DL.Farsi.Sub.Film2Media.mkv" } },
+                        { "epNum": 3, "qualities": { "480p": "https://bnrhd3fvb374s4bkwzxm54xf.abrtech.top/pnot/Series/Tsue.to.Tsurugi.no.Wistoria/S01/Tsue.to.Tsurugi.no.Wistoria.S01E03.mkv", "720p": "https://bnrhd3fvb374s4bkwzxm54xf.abrtech.top/pnot/Series/Tsue.to.Tsurugi.no.Wistoria/S01/Tsue.to.Tsurugi.no.Wistoria.S01E03.720p.WEB-DL.Farsi.Sub.Film2Media.mkv" } },
+                        { "epNum": 4, "qualities": { "480p": "https://bnrhd3fvb374s4bkwzxm54xf.abrtech.top/pnot/Series/Tsue.to.Tsurugi.no.Wistoria/S01/Tsue.to.Tsurugi.no.Wistoria.S01E04.mkv", "720p": "https://bnrhd3fvb374s4bkwzxm54xf.abrtech.top/pnot/Series/Tsue.to.Tsurugi.no.Wistoria/S01/Tsue.to.Tsurugi.no.Wistoria.S01E04.720p.WEB-DL.Farsi.Sub.Film2Media.mkv" } },
+                        { "epNum": 5, "qualities": { "480p": "https://bnrhd3fvb374s4bkwzxm54xf.abrtech.top/pnot/Series/Tsue.to.Tsurugi.no.Wistoria/S01/Tsue.to.Tsurugi.no.Wistoria.S01E05.mkv", "720p": "https://bnrhd3fvb374s4bkwzxm54xf.abrtech.top/pnot/Series/Tsue.to.Tsurugi.no.Wistoria/S01/Tsue.to.Tsurugi.no.Wistoria.S01E05.720p.WEB-DL.Farsi.Sub.Film2Media.mkv" } },
+                        { "epNum": 6, "qualities": { "480p": "https://bnrhd3fvb374s4bkwzxm54xf.abrtech.top/pnot/Series/Tsue.to.Tsurugi.no.Wistoria/S01/Tsue.to.Tsurugi.no.Wistoria.S01E06.mkv", "720p": "https://bnrhd3fvb374s4bkwzxm54xf.abrtech.top/pnot/Series/Tsue.to.Tsurugi.no.Wistoria/S01/Tsue.to.Tsurugi.no.Wistoria.S01E06.720p.WEB-DL.Farsi.Sub.Film2Media.mkv" } },
+                        { "epNum": 7, "qualities": { "480p": "https://bnrhd3fvb374s4bkwzxm54xf.abrtech.top/pnot/Series/Tsue.to.Tsurugi.no.Wistoria/S01/Tsue.to.Tsurugi.no.Wistoria.S01E07.mkv", "720p": "https://bnrhd3fvb374s4bkwzxm54xf.abrtech.top/pnot/Series/Tsue.to.Tsurugi.no.Wistoria/S01/Tsue.to.Tsurugi.no.Wistoria.S01E07.720p.WEB-DL.Farsi.Sub.Film2Media.mkv" } },
+                        { "epNum": 8, "qualities": { "480p": "https://bnrhd3fvb374s4bkwzxm54xf.abrtech.top/pnot/Series/Tsue.to.Tsurugi.no.Wistoria/S01/Tsue.to.Tsurugi.no.Wistoria.S01E08.mkv", "720p": "https://bnrhd3fvb374s4bkwzxm54xf.abrtech.top/pnot/Series/Tsue.to.Tsurugi.no.Wistoria/S01/Tsue.to.Tsurugi.no.Wistoria.S01E08.720p.WEB-DL.Farsi.Sub.Film2Media.mkv" } },
+                        { "epNum": 9, "qualities": { "480p": "https://bnrhd3fvb374s4bkwzxm54xf.abrtech.top/pnot/Series/Tsue.to.Tsurugi.no.Wistoria/S01/Tsue.to.Tsurugi.no.Wistoria.S01E09.mkv", "720p": "https://bnrhd3fvb374s4bkwzxm54xf.abrtech.top/pnot/Series/Tsue.to.Tsurugi.no.Wistoria/S01/Tsue.to.Tsurugi.no.Wistoria.S01E09.720p.WEB-DL.Farsi.Sub.Film2Media.mkv" } },
+                        { "epNum": 10, "qualities": { "480p": "https://bnrhd3fvb374s4bkwzxm54xf.abrtech.top/pnot/Series/Tsue.to.Tsurugi.no.Wistoria/S01/Tsue.to.Tsurugi.no.Wistoria.S01E10.mkv", "720p": "https://bnrhd3fvb374s4bkwzxm54xf.abrtech.top/pnot/Series/Tsue.to.Tsurugi.no.Wistoria/S01/Tsue.to.Tsurugi.no.Wistoria.S01E10.720p.WEB-DL.Farsi.Sub.Film2Media.mkv" } },
+                        { "epNum": 11, "qualities": { "480p": "https://bnrhd3fvb374s4bkwzxm54xf.abrtech.top/pnot/Series/Tsue.to.Tsurugi.no.Wistoria/S01/Tsue.to.Tsurugi.no.Wistoria.S01E11.mkv", "720p": "https://bnrhd3fvb374s4bkwzxm54xf.abrtech.top/pnot/Series/Tsue.to.Tsurugi.no.Wistoria/S01/Tsue.to.Tsurugi.no.Wistoria.S01E11.720p.WEB-DL.Farsi.Sub.Film2Media.mkv" } },
+                        { "epNum": 12, "qualities": { "480p": "https://bnrhd3fvb374s4bkwzxm54xf.abrtech.top/pnot/Series/Tsue.to.Tsurugi.no.Wistoria/S01/Tsue.to.Tsurugi.no.Wistoria.S01E12.mkv", "720p": "https://bnrhd3fvb374s4bkwzxm54xf.abrtech.top/pnot/Series/Tsue.to.Tsurugi.no.Wistoria/S01/Tsue.to.Tsurugi.no.Wistoria.S01E12.720p.WEB-DL.Farsi.Sub.Film2Media.mkv" } }
+                    ]
+                },
+                {
+                    "seasonNumber": 2,
+                    "seasonName": "فصل دوم - 13 قسمت",
+                    "episodes": [
+                        { "epNum": 0, "qualities": { "480p": "https://bnrhd3fvb374s4bkwzxm54xf.abrtech.top/pnot/Series/Tsue.to.Tsurugi.no.Wistoria/S02/Tsue.to.Tsurugi.no.Wistoria.Season2.S02E00.mkv", "720p": "https://bnrhd3fvb374s4bkwzxm54xf.abrtech.top/pnot/Series/Tsue.to.Tsurugi.no.Wistoria/S02/Tsue.to.Tsurugi.no.Wistoria.Season2.S02E00.720p.WEB-DL.Farsi.Sub.Film2Media.mkv" } },
+                        { "epNum": 1, "qualities": { "480p": "https://bnrhd3fvb374s4bkwzxm54xf.abrtech.top/pnot/Series/Tsue.to.Tsurugi.no.Wistoria/S02/Tsue.to.Tsurugi.no.Wistoria.Season2.S02E01.mkv", "720p": "https://bnrhd3fvb374s4bkwzxm54xf.abrtech.top/pnot/Series/Tsue.to.Tsurugi.no.Wistoria/S02/Tsue.to.Tsurugi.no.Wistoria.Season2.S02E01.720p.WEB-DL.Farsi.Sub.Film2Media.mkv" } },
+                        { "epNum": 2, "qualities": { "480p": "https://bnrhd3fvb374s4bkwzxm54xf.abrtech.top/pnot/Series/Tsue.to.Tsurugi.no.Wistoria/S02/Tsue.to.Tsurugi.no.Wistoria.Season2.S02E02.mkv", "720p": "https://bnrhd3fvb374s4bkwzxm54xf.abrtech.top/pnot/Series/Tsue.to.Tsurugi.no.Wistoria/S02/Tsue.to.Tsurugi.no.Wistoria.Season2.S02E02.720p.WEB-DL.Farsi.Sub.Film2Media.New.mkv" } },
+                        { "epNum": 3, "qualities": { "480p": "https://bnrhd3fvb374s4bkwzxm54xf.abrtech.top/pnot/Series/Tsue.to.Tsurugi.no.Wistoria/S02/Tsue.to.Tsurugi.no.Wistoria.Season2.S02E03.mkv", "720p": "https://bnrhd3fvb374s4bkwzxm54xf.abrtech.top/pnot/Series/Tsue.to.Tsurugi.no.Wistoria/S02/Tsue.to.Tsurugi.no.Wistoria.Season2.S02E03.720p.WEB-DL.Farsi.Sub.Film2Media.New.mkv" } },
+                        { "epNum": 4, "qualities": { "480p": "https://bnrhd3fvb374s4bkwzxm54xf.abrtech.top/pnot/Series/Tsue.to.Tsurugi.no.Wistoria/S02/Tsue.to.Tsurugi.no.Wistoria.Season2.S02E04.mkv", "720p": "https://bnrhd3fvb374s4bkwzxm54xf.abrtech.top/pnot/Series/Tsue.to.Tsurugi.no.Wistoria/S02/Tsue.to.Tsurugi.no.Wistoria.Season2.S02E04.720p.WEB-DL.Farsi.Sub.Film2Media.New.mkv" } },
+                        { "epNum": 5, "qualities": { "480p": "https://bnrhd3fvb374s4bkwzxm54xf.abrtech.top/pnot/Series/Tsue.to.Tsurugi.no.Wistoria/S02/Tsue.to.Tsurugi.no.Wistoria.Season2.S02E05.mkv", "720p": "https://bnrhd3fvb374s4bkwzxm54xf.abrtech.top/pnot/Series/Tsue.to.Tsurugi.no.Wistoria/S02/Tsue.to.Tsurugi.no.Wistoria.Season2.S02E05.720p.WEB-DL.Farsi.Sub.Film2Media.New.mkv" } },
+                        { "epNum": 6, "qualities": { "480p": "https://bnrhd3fvb374s4bkwzxm54xf.abrtech.top/pnot/Series/Tsue.to.Tsurugi.no.Wistoria/S02/Tsue.to.Tsurugi.no.Wistoria.Season2.S02E06.mkv", "720p": "https://bnrhd3fvb374s4bkwzxm54xf.abrtech.top/pnot/Series/Tsue.to.Tsurugi.no.Wistoria/S02/Tsue.to.Tsurugi.no.Wistoria.Season2.S02E06.720p.WEB-DL.Farsi.Sub.Film2Media.New.mkv" } },
+                        { "epNum": 7, "qualities": { "480p": "https://bnrhd3fvb374s4bkwzxm54xf.abrtech.top/pnot/Series/Tsue.to.Tsurugi.no.Wistoria/S02/Tsue.to.Tsurugi.no.Wistoria.Season2.S02E07.mkv", "720p": "https://bnrhd3fvb374s4bkwzxm54xf.abrtech.top/pnot/Series/Tsue.to.Tsurugi.no.Wistoria/S02/Tsue.to.Tsurugi.no.Wistoria.Season2.S02E07.720p.WEB-DL.Farsi.Sub.Film2Media.New.mkv" } },
+                        { "epNum": 8, "qualities": { "480p": "https://bnrhd3fvb374s4bkwzxm54xf.abrtech.top/pnot/Series/Tsue.to.Tsurugi.no.Wistoria/S02/Tsue.to.Tsurugi.no.Wistoria.Season2.S02E08.mkv", "720p": "https://bnrhd3fvb374s4bkwzxm54xf.abrtech.top/pnot/Series/Tsue.to.Tsurugi.no.Wistoria/S02/Tsue.to.Tsurugi.no.Wistoria.Season2.S02E08.720p.WEB-DL.Farsi.Sub.Film2Media.New.mkv" } },
+                        { "epNum": 9, "qualities": { "480p": "https://bnrhd3fvb374s4bkwzxm54xf.abrtech.top/pnot/Series/Tsue.to.Tsurugi.no.Wistoria/S02/Tsue.to.Tsurugi.no.Wistoria.Season2.S02E09.mkv", "720p": "https://bnrhd3fvb374s4bkwzxm54xf.abrtech.top/pnot/Series/Tsue.to.Tsurugi.no.Wistoria/S02/Tsue.to.Tsurugi.no.Wistoria.Season2.S02E09.720p.WEB-DL.Farsi.Sub.Film2Media.New.mkv" } },
+                        { "epNum": 10, "qualities": { "480p": "https://bnrhd3fvb374s4bkwzxm54xf.abrtech.top/pnot/Series/Tsue.to.Tsurugi.no.Wistoria/S02/Tsue.to.Tsurugi.no.Wistoria.Season2.S02E10.mkv", "720p": "https://bnrhd3fvb374s4bkwzxm54xf.abrtech.top/pnot/Series/Tsue.to.Tsurugi.no.Wistoria/S02/Tsue.to.Tsurugi.no.Wistoria.Season2.S02E10.720p.WEB-DL.Farsi.Sub.Film2Media.New.mkv" } },
+                        { "epNum": 11, "qualities": { "480p": "https://bnrhd3fvb374s4bkwzxm54xf.abrtech.top/pnot/Series/Tsue.to.Tsurugi.no.Wistoria/S02/Tsue.to.Tsurugi.no.Wistoria.Season2.S02E11.mkv", "720p": "https://bnrhd3fvb374s4bkwzxm54xf.abrtech.top/pnot/Series/Tsue.to.Tsurugi.no.Wistoria/S02/Tsue.to.Tsurugi.no.Wistoria.Season2.S02E11.720p.WEB-DL.Farsi.Sub.Film2Media.New.mkv" } },
+                        { "epNum": 12, "qualities": { "480p": "https://bnrhd3fvb374s4bkwzxm54xf.abrtech.top/pnot/Series/Tsue.to.Tsurugi.no.Wistoria/S02/Tsue.to.Tsurugi.no.Wistoria.Season2.S02E12.mkv", "720p": "https://bnrhd3fvb374s4bkwzxm54xf.abrtech.top/pnot/Series/Tsue.to.Tsurugi.no.Wistoria/S02/Tsue.to.Tsurugi.no.Wistoria.Season2.S02E12.720p.WEB-DL.Farsi.Sub.Film2Media.New.mkv" } }
+                    ]
+                }
+            ]
+        }
    
    
    
